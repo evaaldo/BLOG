9431a0502f654ad02b4d3f6798f1bb4db843caca
evaldo <evaaldormj@gmail.com>
2025-03-15 04:58:46 -0300

    Feat: Add blog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { AboutComponent } from './about/about.component';
+import { PostComponent } from './post/post.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HeaderComponent, AboutComponent],
+  imports: [CommonModule, HeaderComponent, AboutComponent, PostComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
