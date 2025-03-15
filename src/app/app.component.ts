54f3c22f708e6559b22574d372c8c3adae46ea2f
evaldo <evaaldormj@gmail.com>
2025-03-15 05:02:07 -0300

    Feat: Add footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { AboutComponent } from './about/about.component';
 import { PostComponent } from './post/post.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HeaderComponent, AboutComponent, PostComponent],
+  imports: [CommonModule, HeaderComponent, AboutComponent, PostComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
