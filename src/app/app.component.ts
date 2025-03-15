eeaee9299b05112a5b6312270083cc11e761ca6d
evaldo <evaaldormj@gmail.com>
2025-03-15 04:44:29 -0300

    Feat: Add about component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HeaderComponent],
+  imports: [CommonModule, HeaderComponent, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
